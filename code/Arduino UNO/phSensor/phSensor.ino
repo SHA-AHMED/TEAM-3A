89d3d20a6ac5404328674aa60aae9ce0d5ac28ad
Alex Pilkey <bobbobb3746@gmail.com>
2024-04-08 13:28:17 -0400

    code organization

---
+++ code/Arduino UNO/phSensor/phSensor.ino
@@ -0,0 +1,20 @@
+#include "ph_grav.h"
+Gravity_pH pH = Gravity_pH(A0);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // setup serial communication with LoRa transmitter board
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // read ph sensor
+
+  // send ph sensor data to LoRa board
+
+  // sleep? delay?
+
+}
