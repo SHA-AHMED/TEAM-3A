985f23805700eb40ab0a021e6f6574f31f85317e
Alex Pilkey <bobbobb3746@gmail.com>
2024-04-10 10:25:54 -0400

    ph sensor board initial commit

--- code/phSensor/phSensor.ino
+++ code/phSensor/phSensor.ino
@@ -5,6 +5,7 @@ void setup() {
   // put your setup code here, to run once:
 
   // setup serial communication with LoRa transmitter board
+  Serial.begin(9600);
 
 }
 
@@ -12,9 +13,11 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   // read ph sensor
+  float pH_Value = pH.read_ph();
 
   // send ph sensor data to LoRa board
 
   // sleep? delay?
+  delay(1000);
 
 }
